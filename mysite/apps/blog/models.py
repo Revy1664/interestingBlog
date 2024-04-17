97d0ff5c53696da5ad179f1feb1aa4563d1e2747
Revy1664 <steinsgate95@mail.ru>
2024-04-17 21:52:59 +0600

    added media, wrote styles for posts page

--- mysite/apps/blog/models.py
+++ mysite/apps/blog/models.py
@@ -5,6 +5,7 @@ from django.utils import timezone
 
 class Post(models.Model):
 	author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	image = models.ImageField(upload_to="post-images/", blank=True)
 	title = models.CharField(max_length=50)
 	text = models.TextField()
 	publication_date = models.DateTimeField(default=timezone.now)
